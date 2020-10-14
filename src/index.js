9c3da1c340fc775f8e3d9189e9a4fc4dfb1fa865
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-10-15 00:29:29 +0900

    webpackの概要についてまたwebpackを使用しないパターンの再現

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const conpornent = () => {
+    const element = document.createElement('div');
+    const array = ['hello', 'webpack']
+    element.innerHTML = _.join(array, ' ')
+    return element;
+}
+
+document.body.appendChild(conpornent());
