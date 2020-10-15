f6fa3517aa4a83b274e5038c2ebab958f3193135
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-10-15 12:32:56 +0900

    webpackを実際に使ってみたと簡単に概要まとめ

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ console.log(outputPath)
 module.export = {
     mode: 'development',
     entry: './src/index.js',
+    output: {
         file_name: './src/main.js',
         path: outputPath
     }
